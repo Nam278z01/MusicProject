ad57cf8c74c6e232d295284c0f1564216e49a17d
Nam Nguyễn <nam278z01@gmail.com>
2021-12-18 20:08:51 +0700

    admin

---
+++ MusicProject/Areas/Admin/Controllers/AccUserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicBus;
+using MusicObj;
+
+namespace MusicProject.Areas.Admin.Controllers
+{
+    public class AccUserController : Controller
+    {
+        // GET: Admin/AccUser
+        public ActionResult IAUndex()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ MusicProject/Areas/Admin/Controllers/AlbumController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicBus;
+using MusicObj;
+
+namespace MusicProject.Areas.Admin.Controllers
+{
+    public class AlbumController : Controller
+    {
+        // GET: Admin/Album
+        AlbumBus ab = new AlbumBus();
+        public ActionResult IALndex()
+        {
+            return View("Index");
+        }
+        //public ActionResult GetAlbum()
+        //{
+        //    List<Album> List = ab.GetAlbumAll();
+        //    return Json(List, JsonRequestBehavior.AllowGet);
+        //}
+    }
+}
