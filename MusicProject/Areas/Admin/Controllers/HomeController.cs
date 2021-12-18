8ee66721456fb1a72a2ca0cdf7d2116a2db41871
Nam Nguyễn <nam278z01@gmail.com>
2021-12-18 22:34:59 +0700

    push nè

--- MusicProject/Areas/Admin/Controllers/HomeController.cs
+++ MusicProject/Areas/Admin/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace MusicProject.Areas.Admin.Controllers
     {
         // GET: Admin/Home
         // Nam !31231231
+        //Nam này!
         public ActionResult Index()
         {
             return View();
